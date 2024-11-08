b5649c8fa41a18d3b0cf8facd2a2deee80189419
Tekatoji <22520190@gm.uit.edu.vn>
2024-11-08 22:14:08 +0700

    Enhance AskQuestion page styling; add input container classes and update textarea focus styles

--- src/pages/AskQuestion/AskQuestion.jsx
+++ src/pages/AskQuestion/AskQuestion.jsx
@@ -5,7 +5,7 @@ const AskQuestion = () => {
   return (
     <div className="max-w-4xl mx-auto p-6 gap-6 flex flex-col">
       <h1 className="font-bold text-2xl">Ask a public question</h1>
-      <div className="border rounded p-4 shadow">
+      <div className="askquestion-input-container">
         <label className="askquestion-input-tittle" htmlFor="title">
           Title
         </label>
@@ -13,54 +13,53 @@ const AskQuestion = () => {
           Be specific and imagine you’re asking a question to another person.
         </span>
         <input
-          className="mt-2 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          className="focus:outline-orange-200 focus:outline-none focus:border-slate-400 mt-2 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:shadow-outline"
           id="title"
           type="text"
-          placeholder="Be specific and imagine you’re asking a question to another person."
+          placeholder="eg. Is there a R function for finding the index of an element in a vector?"
         />
       </div>
-      <div className="">
-        <label
-          className="block text-gray-700 text-sm font-bold mb-2"
-          htmlFor="details"
-        >
+      <div className="askquestion-input-container">
+        <label className="askquestion-input-tittle" htmlFor="details">
           What are the details of your problem?
         </label>
+        <span className="askquestion-input-explain-tittle">
+          Introduce the problem and expand on what you put in the title. Minimum
+          20 characters.
+        </span>
         <textarea
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline h-32"
+          className="focus:outline-orange-200 focus:outline-none focus:border-slate-400 mt-2 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline h-32"
           id="details"
-          placeholder="Introduce the problem and expand on what you put in the title. Minimum 20 characters."
-        >
-          uádawdúadúawadsdwasdw
-        </textarea>
+          placeholder=""
+        ></textarea>
       </div>
-      <div className="">
-        <label
-          className="block text-gray-700 text-sm font-bold mb-2"
-          htmlFor="expectations"
-        >
+      <div className="askquestion-input-container">
+        <label className="askquestion-input-tittle" htmlFor="expectations">
           What did you try and what were you expecting?
         </label>
+        <span className="askquestion-input-explain-tittle">
+          Describe what you tried, what you expected to happen, and what
+          actually resulted. Minimum 20 characters.
+        </span>
         <textarea
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline h-32"
+          className="focus:outline-orange-200 focus:outline-none focus:border-slate-400 mt-2 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline h-32"
           id="expectations"
-          placeholder="Describe what you tried, what you expected to happen, and what actually resulted. Minimum 20 characters."
-        >
-          dúawasdwasdwasdwasdwasd
-        </textarea>
+          placeholder=""
+        ></textarea>
       </div>
-      <div className="mb-6">
-        <label
-          className="block text-gray-700 text-sm font-bold mb-2"
-          htmlFor="tags"
-        >
+      <div className="askquestion-input-container">
+        <label className="askquestion-input-tittle" htmlFor="tags">
           Tags
         </label>
+        <span className="askquestion-input-explain-tittle">
+          Add up to 5 tags to describe what your question is about. Separate
+          tags with spaces.
+        </span>
         <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          className="focus:outline-orange-200 focus:outline-none focus:border-slate-400 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           id="tags"
           type="text"
-          placeholder="Add up to 5 tags to describe what your question is about. Separate tags with spaces."
+          placeholder="e.g React JavaScript TailWindCSS"
         />
       </div>
       <div className="flex items-center justify-between">
